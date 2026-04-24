c6fae239b4bd7868eca33eb4c8cabce19cf15f9c
Alvaro <alvaromarini1411@gmail.com>
2026-04-23 21:41:03 -0300

    feat(database): agrega tabla de integrantes

--- database/init.sql
+++ database/init.sql
@@ -7,10 +7,3 @@ CREATE TABLE IF NOT EXISTS members (
     servicio VARCHAR(50),
     estado VARCHAR(20)
 );
-
-INSERT INTO members (nombre, apellido, legajo, feature, servicio, estado) VALUES
-('Valentino', 'Chiappini', '33072', 'Feature 01', 'Coordinador', 'Activo'),
-('Sergio Adrian', 'Maldonado', '21352', 'Feature 02', 'Frontend', 'Activo'),
-('Juan Ignacio', 'Wilt', '33151', 'Feature 03', 'Backend', 'Activo'),
-('Alvaro', 'Marini', '33133', 'Feature 04', 'Database', 'Activo'),
-('Valentino', 'Chiappini', '33072', 'Feature 05', 'Portainer', 'Activo');
